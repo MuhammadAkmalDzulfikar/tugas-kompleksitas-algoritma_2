f06a08e546c55c012f464f98342ae7c6dc432d4b
zul <161696682+MuhammadAkmalDzulfikar@users.noreply.github.com>
2024-10-01 21:46:43 +0700

    Add files via upload

---
+++ TugasKompleksitasAlgoritma_Akmal_20230040069.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaskompleksitasalgoritma_akmal_20230040069;
+
+/**
+ *
+ * @author Admin
+ */
+public class TugasKompleksitasAlgoritma_Akmal_20230040069 {
+
+public static void main(String[] args) {
+        int bilangan = 30;
+        int hasil = 0;
+
+        for (int i = 0;i <2; i++) {
+            hasil += bilangan;
+        }
+        
+        System.out.println("Hasil dari " + bilangan + "pangkat dua adalah:" + hasil);
+    }
+    
+}
+
+// Analisa kompleksitas algoritma dari pemrograman ini
+// -Waktu (Time Comlexity): Komplesitas waktu adalah 0(1), karena operasi perkalian dilakukan sekali
+// -Ruang (Space Complexity): Komplesitas ruang adalah 0(1), karena hanya memerlukan ruang untuk variabel number dari hasil
+
+//Simulasi perhitungan data : pada setiap iterasi program menambahkan nilai number kedalam variabel bilangan
+}
+
+   
+    
+}
